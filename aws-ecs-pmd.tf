5686c88ea42e49c070936f92af13d1526153151c
Leandro Gonçalves de Oliveira <olegon.main@gmail.com>
2022-12-30 16:07:18 -0300

    Renaming ECS Task Definition family name.

--- aws-ecs-pmd.tf
+++ aws-ecs-pmd.tf
@@ -1,5 +1,5 @@
 resource "aws_ecs_task_definition" "pmd" {
-  family                   = "service"
+  family                   = "apex-pmd"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
   cpu                      = 256
